b90d40227d745ca42136f3a217e0562895ee52ad
Jeyhun <106176888+im-Jeyhun@users.noreply.github.com>
2022-10-07 22:17:10 -0700

    Add files via upload

---
+++ dropMenuAndSliderWithJQuery/app.js
@@ -0,0 +1,68 @@
+let buttons = document.querySelectorAll('.up-menu li a');
+
+let menus = document.querySelectorAll('.up-menu ul');
+
+buttons.forEach(btn => {
+
+  btn.onclick = function(e) {
+
+    e.preventDefault();
+    let downmenu = this.nextElementSibling;
+
+   
+
+
+      $(downmenu).slideToggle()
+      
+ 
+
+    
+  }
+
+  for(let menu of menus) {
+    $(menu).hide();
+  }
+}) 
+
+
+
+
+
+
+$('.slide').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
